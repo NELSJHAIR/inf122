7dc8ee454c3805dd885c570ac292af357231bdee
NELSJHAIR <155449859+NELSJHAIR@users.noreply.github.com>
2024-01-23 13:58:12 +0000

    .

---
+++ my-app/src/app/contador/components/contador.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/Contador.css";
+function Contador({ nroClicks, mostrar }) {
+    return (
+        mostrar ? <div className='contador'>{nroClicks}</div> : null
+    );
+}
+export default Contador;
--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,10 +1,62 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Componente from "./Ejemplo/Ejmplo";
-export default function Home() {
+<import './App.css';
+import Boton from './componentes/components/boton';
+import Contador from './componentes/components/Contador';
+import { useState } from 'react'; /* para guardarn estados */
+
+
+function App() {
+  
+  const [ nroClicks, setNumClicks] = useState(0);
+  const [show, setShow] = useState(true);
+  const click = () => {
+    setNumClicks(nroClicks+1);
+    console.log(nroClicks);
+  }
+  const reiniciar = () => {
+    setNumClicks(0);
+    console.log("Reiniciar");
+  }
+
+  const tresMas =() => {
+    setNumClicks(nroClicks+3);
+  }
+  const menosTres =()=>{
+    setNumClicks(nroClicks-3);
+  }
+  const masUno =()=>{
+    setNumClicks(nroClicks+1);
+  }
+  const menosUno =()=>{
+    setNumClicks(nroClicks-1);
+  }
+  const mostrar = () => {
+    setShow(!show);
+  }
   return (
-    <h1>Hola</h1>
-    <Componente/>
+    <div className="App">
+      <div className="contenedor-principal">
+      <Contador nroClicks={nroClicks} mostrar={show} />
+        <div className='contendedor-botones'>
+          <Boton texto="Click" esBotonClick={true}
+            funcionClick={click} />
+          <Boton texto="Reiniciar" esBotonClick={false}
+            funcionClick={reiniciar} />
+          <Boton texto="Mostrar/Ocultar" esBotonClick={false} funcionClick={mostrar} />
+        <div className="contenedor-botones2">
+          <Boton texto="-3" esBotonClick={true}
+            funcionClick={menosTres}/>
+          <Boton texto="+3" esBotonClick={true}
+            funcionClick={tresMas}/>
+          <Boton texto="-1" esBotonClick={true}
+            funcionClick={menosUno}/>
+          <Boton texto="+1" esBotonClick={true}
+            funcionClick={masUno}/>
+        </div>
+        
+        </div>
+      </div>
+    </div>
   );
 }
 
+export default App;
