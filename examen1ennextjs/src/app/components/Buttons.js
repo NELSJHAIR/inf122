791d5118f0b474ffc5af3b9dbe09399ddbaf7384
NELSJHAIR <nelsjhairsaireduran2003@gmail.com>
2024-01-27 10:00:19 -0400

    recuperatorio

--- examen1ennextjs/src/app/components/Buttons.js
+++ examen1ennextjs/src/app/components/Buttons.js
@@ -5,9 +5,9 @@ import style from './Button.module.css';
 function Buttons () {
   return (
     <div className={style.buttons}>
-        <button className={style.btnPrimary} onclick="download()">
+        <button className={style.btnPrimary}>
             <div className={style.btnContent}>
-                <p>Download CV</p>
+                <a href='https://www.canva.com/design/DAF4Zhy4cX8/XvJismg-C25G-8yHSmn0Fg/view?utm_content=DAF4Zhy4cX8&utm_campaign=designshare&utm_medium=link&utm_source=editor'>Download CV</a>
                     <Image src="/Bottom Alignment.svg" alt="arrow" width={28} height={24}/>
             </div>
         </button>
